8238b411e94673a586d2d058ebd5a111b4c6291f
Timsy Magnus <tmherron09@gmail.com>
2020-08-10 10:00:21 -0500

    Add MyLinq class template

---
+++ Linq_Problems/MyLinq.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Problems
+{
+    public class MyLinq
+    {
+
+
+        public List<string> WordsWithTH(List<string> words)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+
+        public List<string> NoDuplicates(List<string> words)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public double ClassAverageWithoutLowestGrade(List<string> classGrades)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AlphabeticalFrequency(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
